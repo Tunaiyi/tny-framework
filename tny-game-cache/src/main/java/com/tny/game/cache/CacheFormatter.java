9396d0a758b079e5a4f3f65029152d0867a3b0bb
yangkun <KGTny@sg.tny.net>
2016-10-22 14:11:53 +0800

    Change : CacheFormatter 解析处理

--- tny-game-cache/src/main/java/com/tny/game/cache/CacheFormatter.java
+++ tny-game-cache/src/main/java/com/tny/game/cache/CacheFormatter.java
@@ -1,11 +1,17 @@
 package com.tny.game.cache;
 
+import com.tny.game.LogUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @SuppressWarnings("unchecked")
 public abstract class CacheFormatter<O, D> implements CacheTrigger<O, D, Object> {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(CacheFormatter.class);
 
     public abstract Object format2Save(String key, O object);
 
@@ -40,24 +46,43 @@ public abstract class CacheFormatter<O, D> implements CacheTrigger<O, D, Object>
             Collection<D> objects = (Collection<D>) object;
             List<Object> list = new ArrayList<>();
             for (D value : objects) {
-                list.add(this.format2Load(key, value));
+                try {
+                    list.add(this.format2Load(key, value));
+                } catch (Throwable e) {
+                    LOGGER.error(LogUtils.format("parse {} exception", key), e);
+                }
             }
             return objects;
         } else {
-            return this.format2Load(key, object);
+            try {
+                return this.format2Load(key, object);
+            } catch (Throwable e) {
+                LOGGER.error(LogUtils.format("parse 2 Load {} exception", key), e);
+            }
+            return null;
         }
     }
+
 
     public Object toSave(String key, O object) {
         if (object instanceof Collection) {
             Collection<O> objects = (Collection<O>) object;
             List<Object> list = new ArrayList<>();
             for (O value : objects) {
-                list.add(this.format2Save(key, value));
+                try {
+                    list.add(this.format2Save(key, value));
+                } catch (Throwable e) {
+                    LOGGER.error(LogUtils.format("format {} exception", key), e);
+                }
             }
             return objects;
         } else {
-            return this.format2Save(key, object);
+            try {
+                return this.format2Save(key, object);
+            } catch (Throwable e) {
+                LOGGER.error(LogUtils.format("format {} exception", key), e);
+            }
+            return null;
         }
     }
 
