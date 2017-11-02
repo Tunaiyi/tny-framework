fbb56b8a1a4ebad51d2e3c2301f679324d7ba5e1
kun.y <yangkun311@gmail.com>
2017-11-03 01:06:19 +0800

    Change :IDAide.java

--- tny-game-suite/src/main/java/com/tny/game/suite/login/IDAide.java
+++ tny-game-suite/src/main/java/com/tny/game/suite/login/IDAide.java
@@ -11,10 +11,12 @@ public class IDAide {
 
     public static long PLAYER_ID_OFFSET = 10000000;
 
-    private static char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-            'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P',
-            'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z'};
+    private static char[] digits = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z'};
 
     public static boolean isSystem(long id) {
         return id < PLAYER_ID_OFFSET;
