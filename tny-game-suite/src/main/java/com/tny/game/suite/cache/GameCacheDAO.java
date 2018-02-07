94dead661bafe61eeea2b7a2c2c7cd2a522b3c20
kun.y <yangkun311@gmail.com>
2018-02-07 18:52:53 +0800

    Fixed :

--- tny-game-suite/src/main/java/com/tny/game/suite/cache/GameCacheDAO.java
+++ tny-game-suite/src/main/java/com/tny/game/suite/cache/GameCacheDAO.java
@@ -40,14 +40,15 @@ public interface GameCacheDAO extends CacheDAO, ShardCacheDAO {
     int[] add(@ShardBy("item") @SQLParam("i") Collection<? extends DBCacheItem> items);
 
     @Override
-    @SQL("REPLACE INTO " + ITEM_TABLE + " (" + FULL_FIELD + ") VALUES (:i.key, :i.flags, :i.data, :i.expire, :i.saveAt, :i.uid, :i.itemID, :i.number)")
+    @SQL("INSERT INTO " + ITEM_TABLE + " (" + FIELD + ") VALUES (:i.key, :i.flags, :i.data, :i.expire, :i.saveAt) ON DUPLICATE KEY UPDATE " +
+            "`data`=VALUES(`data`), `flags`=VALUES(`flags`), `expire`=VALUES(`expire`), `saveAt`=VALUES(`saveAt`)")
     int set(@SQLParam("i") DBCacheItem item);
 
     @Override
-    @SQL("REPLACE INTO " + ITEMS_TABLE + " (" + FULL_FIELD + ") VALUES (:i.key, :i.flags, :i.data, :i.expire, :i.saveAt, :i.uid, :i.itemID, :i.number)")
-    int[] set(@ShardBy("item") @SQLParam("i") Collection<? extends DBCacheItem> items);
+    @SQL("INSERT INTO " + ITEM_TABLE + " (" + FIELD + ") VALUES (:i.key, :i.flags, :i.data, :i.expire, :i.saveAt) ON DUPLICATE KEY UPDATE " +
+            "`data`=VALUES(`data`), `flags`=VALUES(`flags`), `expire`=VALUES(`expire`), `saveAt`=VALUES(`saveAt`)")
+    int[] set(@ShardBy @SQLParam("i") Collection<? extends DBCacheItem> items);
 
-    @Override
     @SQL("UPDATE " + ITEM_TABLE + " SET  `data`=:i.data, `flags`=:i.flags, `expire`=:i.expire, `saveAt`=:i.saveAt, `number`=:i.number WHERE `key` = :i.key")
     int update(@SQLParam("i") DBCacheItem item);
 
