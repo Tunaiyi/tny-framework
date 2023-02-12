9f24366083bf9ac4298af20ade450a65d6ecbcbf
YangKun <yangkun311@gmail.com>
2023-02-12 10:16:44 +0800

    refactor:
    - 重构, 重命名

--- tny-game-net/src/main/java/com/tny/game/net/relay/link/GeneralServerRelayTunnel.java
+++ tny-game-net/src/main/java/com/tny/game/net/relay/link/GeneralServerRelayTunnel.java
@@ -52,8 +52,8 @@ public class GeneralServerRelayTunnel<UID> extends ServerTunnel<UID, NetSession<
     @Override
     public MessageWriteFuture relay(RpcTransferContext rpcContext, boolean needPromise) {
         MessageWriteFuture promise = needPromise ? new MessageWriteFuture() : null;
-        rpcContext.transfer(transporter.getLink(), RpcTransactionContext.relayOperation(rpcContext.netMessage()));
-        this.write(rpcContext.netMessage(), promise);
+        rpcContext.transfer(transporter.getLink(), RpcTransactionContext.relayOperation(rpcContext.getMessage()));
+        this.write(rpcContext.getMessage(), promise);
         rpcContext.completeSilently();
         return promise;
     }
